14773b297cc9ee4890c83685a3d82ae91ed40c21
charlie <charlie@localhost>
2018-01-21 20:02:53 -0500

    Cambio de version spring boot

--- DocumentationApi/src/main/java/com/home/documentation/api/json/JsonDateSerializer.java
+++ DocumentationApi/src/main/java/com/home/documentation/api/json/JsonDateSerializer.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonGenerator;
